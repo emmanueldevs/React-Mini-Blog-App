8ecf737aa8fc4f786b76e7d78994e0faaf7659d1
Emmanuel Adeboje <emmanueladeboje@gmail.com>
2020-05-21 12:30:35 +0100

    mini blog app with react

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere delectus odit facilis illo excepturi possimus minima voluptatem quaerat debitis, dolorum harum molestiae eos, consequuntur minus natus esse ea aliquam nisi.</p>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Contact = (props) => {
+    //setTimeout(() => {
+    //    props.history.push('/about')
+    //}, 2000);
+    return (
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere delectus odit facilis illo excepturi possimus minima voluptatem quaerat debitis, dolorum harum molestiae eos, consequuntur minus natus esse ea aliquam nisi.</p>
+        </div>
+    )
+}
+
+export default Contact;
